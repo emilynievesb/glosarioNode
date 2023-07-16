5b6e022a66132791b22b069ad94a2e49fbe727f5
emilynievesb <nemily1710@gmail.com>
2023-07-16 18:51:13 -0500

    feat: :sparkles: Documentación process
    Explicación process y ejemplos

--- index.js
+++ index.js
@@ -11,3 +11,15 @@
 // - NVM es una herramienta muy útil para gestionar múltiples versiones de Node.js en un mismo sistema.
 // - Permite instalar, cambiar y administrar fácilmente las versiones de Node.js que utilizas en tus proyectos.
 // - Por ejemplo, puedes utilizar NVM para cambiar a la versión 14 de Node.js ejecutando "nvm use 14".
+// --------------------------------------
+// PROCESS
+
+// Objeto global process
+// - En Node.js, el objeto global "process" proporciona información y control sobre el proceso actual en ejecución.
+// - Es especialmente útil para acceder a argumentos de línea de comandos, variables de entorno y manejar la salida del programa.
+// - Por ejemplo, puedes acceder a los argumentos de línea de comandos a través de "process.argv", que es un array que contiene los argumentos pasados al programa.
+// Ejemplo:
+// node archivo.js argumento1 argumento2
+// console.log(process.argv); // Output: ['node', 'archivo.js', 'argumento1', 'argumento2']
+
+// --------------------------------------
