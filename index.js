6f2fd57efed5b172a03c576ea15a3348d8badb9d
emilynievesb <nemily1710@gmail.com>
2023-07-16 18:40:59 -0500

    feat: :sparkles: Documentación node
    NVM

---
+++ index.js
@@ -0,0 +1,13 @@
+// --------------------------------------
+// NODE.JS
+
+// Características de Node.js
+// - Node.js es un entorno de ejecución del lado del servidor basado en el motor V8 de Chrome.
+// - Permite ejecutar JavaScript fuera del navegador, lo que lo hace ideal para crear aplicaciones web y servidores.
+// - Una de las características clave de Node.js es su modelo de programación orientado a eventos y no bloqueante.
+//   Esto significa que puede manejar múltiples conexiones simultáneas sin bloquear el hilo principal de ejecución.
+
+// NVM (Node Version Manager)
+// - NVM es una herramienta muy útil para gestionar múltiples versiones de Node.js en un mismo sistema.
+// - Permite instalar, cambiar y administrar fácilmente las versiones de Node.js que utilizas en tus proyectos.
+// - Por ejemplo, puedes utilizar NVM para cambiar a la versión 14 de Node.js ejecutando "nvm use 14".
